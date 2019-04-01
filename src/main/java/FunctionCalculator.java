9f0b7f91e4a3b0c9b8d5f9a7574f7e76c81bed9d
Vitalii Putiakhin <putyahin@gmail.com>
2019-04-01 12:55:41 +0300

    Added errors check in FunctionCalculator

--- src/main/java/FunctionCalculator.java
+++ src/main/java/FunctionCalculator.java
@@ -1,318 +1,350 @@
 
 /*
-Приоритеты операций:
-1.Скобки
-2.Унарное сложение вычитание
-3.Возведение в степень, функции
-4.Умножение, деление
-5.Сложение, вычитание
+Priority of operations:
+1.Braces
+2.Unary minus and plus
+3.Math power and functions like sinus etc.
+4.Multiply, divide
+5.Plus, minus
 */
-
 
 import java.util.ArrayList;
 import java.util.Stack;
 
 public class FunctionCalculator {
-    private String function; // This is our function f(x) which will be calculated
-    private double x; // This is our value of x  - argument of function f(x)
-    private ArrayList<String> error; //Errors
-    private int offset;
-    private double result;
-    private char op;
-    private double object;
-    private char[] expression; // Function f(x) presented as array of chars
-    private boolean noObj;
-    private Stack<Character> opStack;
-    private Stack<Double> objectStack;
-    private Stack<Integer> offsetStack;
+	private String function; // This is our function f(x) which will be calculated
+	private double x; // This is our value of x - argument of function f(x)
+	private ArrayList<String> error; // Errors
+	private int offset;
+	private double result;
+	private char op;
+	private double object;
+	private char[] expression; // Function f(x) presented as array of chars
+	private boolean noObj;
+	private Stack<Character> opStack;
+	private Stack<Double> objectStack;
+	private Stack<Integer> offsetStack;
 
-    public FunctionCalculator(String function, double x) {
-        this.function = function;
-        this.x = x;
-        error = new ArrayList<>();
-    }
+	public FunctionCalculator() {
+		error = new ArrayList<>();
+	}
 
-    public double Calculator() {
-        function=function.toLowerCase();
-        function=function.replaceAll(" ","");
-        result = 0.0;
-        object = 0.0;
-        offset = 0;
-        expression = function.toCharArray();
-        op = '+';
-        noObj = true;
-        opStack = new Stack<>();
-        objectStack = new Stack<>();
-        offsetStack = new Stack<>();
-        System.out.println(expression);
-        return Calculate();
-    }
+	public FunctionCalculator(String function, double x) {
+		this.function = function;
+		this.x = x;
+		error = new ArrayList<>();
 
-    public double Calculate() {
+	}
 
-        result = ExpressionPriority5();
-        return result;
-    }
-    public boolean CheckBraces() {
-        boolean ok=false;
-        int braces=0;
-        for(int i=0;i<function.length();i++) {
-            switch (function.charAt(i)) {
-                case '(':
-                    braces++;
-                    break;
-                case ')':
-                    braces--;
-                    break;
-            }
-        }
-        if (braces==0) ok=true;
+	public void setFunction(String function) {
+		this.function = function;
+	}
 
-        return ok;
-    }
-    public double GetNumber() {
-        double result = 0.0;
-        int k = 0;
-        while (Character.isDigit(expression[offset + k]) || expression[offset + k] == '.') {
-            k++;
-            if (offset + k > expression.length - 1) break;
-        }
-        try {
-            result = Double.parseDouble(function.substring(offset, offset + k));
-            offset = offset + k;
-            System.out.println("Number=" + result + " Offset " + offset);
-        } catch (Exception e) {
-            error.add("Invalid number");
-            System.out.println("Error");
-            offset = function.length() - 1;
-        }
-        return result;
-    }
+	public void setX(double x) {
+		this.x = x;
+	}
 
-    public double GetX() {
-        double result = x;
-        offset++;
-        System.out.println("X is " + result);
-        return result;
-    }
+	public String getFunction() {
+		return this.function;
+	}
 
-    public void GetObject() {
-        System.out.println("Offset " + offset);
-        if (offset <= function.length() - 1) {
-            if (Character.isDigit(expression[offset])) {
-                object = GetNumber();
-                noObj = false;
-            } else
-                switch (expression[offset]) {
-                    case '+':
-                        op = '+';
-                        offset++;
-                        System.out.println("Case +");
-                        break;
-                    case '-':
-                        op = '-';
-                        offset++;
-                        System.out.println("Case -");
-                        break;
-                    case '*':
-                        op = '*';
-                        offset++;
-                        System.out.println("Case *");
-                        break;
-                    case '/':
-                        op = '/';
-                        offset++;
-                        System.out.println("Case /");
-                        break;
-                    case '^':
-                        op = '^';
-                        //offset++;
-                        System.out.println("Case ^");
-                        break;
-                    case 's':
-                        op = 'f';
-                        break;
-                    case 'c':
-                        op = 'f';
-                        break;
-                    case 't':
-                        op = 'f';
-                        break;
-                    case 'l':
-                        op = 'f';
-                        break;
-                    case 'p':
-                        offset++;
-                        if (offset <= function.length() - 1) {
-                            if (expression[offset] == 'i') {
-                                object = Math.PI;
-                                offset++;
-                                noObj = false;
-                            } else error.add("Invalid function");
-                        } else {
-                            error.add("Invalid function");
-                        }
-                        break;
-                    case 'e':
-                        object = Math.E;
-                        offset++;
-                        noObj = false;
-                        break;
-                    case '(':
-                        break;
-                    case ')':
-                        op = '+';
-                        object = 0.0;
-                        break;
-                    case 'x':
-                        object = GetX();
-                        noObj = false;
-                        break;
-                }
+	public double getX() {
+		return this.x;
+	}
 
-        }
+	public double Calculator() {
+		function = function.toLowerCase();
+		function = function.replaceAll(" ", "");
+		result = 0.0;
+		object = 0.0;
+		offset = 0;
+		expression = function.toCharArray();
+		op = '+';
+		noObj = true;
+		opStack = new Stack<>();
+		objectStack = new Stack<>();
+		offsetStack = new Stack<>();
+		return Calculate();
+	}
 
-    }
+	private double Calculate() {
 
+		result = ExpressionPriority5();
+		return result;
+	}
 
-    public double ExpressionPriority1() {
-        double result = 0.0;
-        if (offset <= function.length() - 1) {
-            if (expression[offset] == '(') {
-                offset++;
-                opStack.push(op);
-                objectStack.push(object);
-                op = '+';
-                object = 0.0;
-                System.out.println("push");
-                result = ExpressionPriority5();
-                op = opStack.pop();
-                object = objectStack.pop();
-                offset = offsetStack.pop();
-                System.out.println("pop, offset =" + offset + " object =" + object + " op =" + op + " result =" + result);
-                GetObject();
+	public boolean CheckBraces() {
+		boolean ok = false;
+		int braces = 0;
+		for (int i = 0; i < function.length(); i++) {
+			switch (function.charAt(i)) {
+			case '(':
+				braces++;
+				break;
+			case ')':
+				braces--;
+				break;
+			}
+		}
+		if (braces == 0)
+			ok = true;
 
-            } else if (expression[offset] == ')') {
-                offsetStack.push(++offset);
-                offset = expression.length;
-                result = object;
+		return ok;
+	}
 
-            } else {
+	public boolean CheckOp() {
+		boolean ok = true;
+		char[] exp;
+		exp = function.toCharArray();
+		offset = 0;
+		object = 0.0;
+		for (int i = 0; i < exp.length - 1; i++) {
+			op = '0';
+			if (exp[i] == '+' || exp[i] == '-' || exp[i] == '/' || exp[i] == '*' || exp[i] == '^') {
+				offset = i + 1;
+				GetObject();
+				if (op != '0')
+					ok = false;
+			}
+			if (exp[i]=='(') {
+				offset = i + 1;
+				GetObject();
+				if (op != '0' && op!='-')
+					ok = false;
+			}
+				
+		}
+		return ok;
+	}
 
-                GetObject();
-                result = object;
-            }
-        } else result = object;
-        return result;
-    }
+	private double GetNumber() {
+		double result = 0.0;
+		int k = 0;
+		while (Character.isDigit(expression[offset + k]) || expression[offset + k] == '.') {
+			k++;
+			if (offset + k > expression.length - 1)
+				break;
+		}
+		try {
+			result = Double.parseDouble(function.substring(offset, offset + k));
+			offset = offset + k;
+		} catch (Exception e) {
+			error.add("Invalid number");
+			offset = function.length() - 1;
+		}
+		return result;
+	}
 
-    public double ExpressionPriority2() {
-        //double result = ExpressionPriority1();
-        if (noObj && op == '-') {
-            GetObject();
-            return -ExpressionPriority1();
-        }
+	private double GetX() {
+		double result = x;
+		offset++;
+		return result;
+	}
 
-        return ExpressionPriority1();
-    }
+	private void GetObject() {
+		if (offset <= function.length() - 1) {
+			if (Character.isDigit(expression[offset])) {
+				object = GetNumber();
+				noObj = false;
+			} else
+				switch (expression[offset]) {
+				case '+':
+					op = '+';
+					offset++;
+					break;
+				case '-':
+					op = '-';
+					offset++;
+					break;
+				case '*':
+					op = '*';
+					offset++;
+					break;
+				case '/':
+					op = '/';
+					offset++;
+					break;
+				case '^':
+					op = '^';
+					break;
+				case 's':
+					op = 'f';
+					break;
+				case 'c':
+					op = 'f';
+					break;
+				case 't':
+					op = 'f';
+					break;
+				case 'l':
+					op = 'f';
+					break;
+				case 'p':
+					offset++;
+					if (offset <= function.length() - 1) {
+						if (expression[offset] == 'i') {
+							object = Math.PI;
+							offset++;
+							noObj = false;
+						} else
+							error.add("Invalid function");
+					} else {
+						error.add("Invalid function");
+					}
+					break;
+				case 'e':
+					object = Math.E;
+					offset++;
+					noObj = false;
+					break;
+				case '(':
+					break;
+				case ')':
+					op = '+';
+					object = 0.0;
+					break;
+				case 'x':
+					object = GetX();
+					noObj = false;
+					break;
+				}
 
-    public double ExpressionPriority3() {
-        double result = ExpressionPriority2();
-        System.out.println("^");
-        while ((op == '^') || op == 'f') {
-            if (offset > function.length() - 1) break;
-            if (expression[offset] == '^') {
-                offset++;
-                GetObject();
-                double temp = ExpressionPriority2();
-                result = Math.pow(result, temp);
-                System.out.println("^, result=" + result + " Offset " + offset);
-            } else if (offset < function.length() - 3) {
-                if ("sin".equals(function.substring(offset, offset + 3))) {
-                    offset += 3;
-                    GetObject();
-                    double temp = ExpressionPriority2();
-                    result = Math.sin(temp);
-                } else if ("cos".equals(function.substring(offset, offset + 3))) {
-                    offset += 3;
-                    GetObject();
-                    double temp = ExpressionPriority2();
-                    result = Math.cos(temp);
-                } else if ("tan".equals(function.substring(offset, offset + 3))) {
-                    offset += 3;
-                    GetObject();
-                    double temp = ExpressionPriority2();
-                    result = Math.tan(temp);
-                } else if ("log".equals(function.substring(offset, offset + 3))) {
-                    offset += 3;
-                    GetObject();
-                    double temp = ExpressionPriority2();
-                    result = Math.log(temp);
-                } else if (offset < function.length() - 4) {
-                    if ("sqrt".equals(function.substring(offset, offset + 4))) {
-                        offset += 4;
-                        GetObject();
-                        double temp = ExpressionPriority2();
-                        result = Math.sqrt(temp);
-                    }
-                }
-            } else error.add("Invalid function");
+		}
 
+	}
 
-        }
-        return result;
-    }
+	private double ExpressionPriority1() {
+		double result = 0.0;
+		if (offset <= function.length() - 1) {
+			if (expression[offset] == '(') {
+				offset++;
+				opStack.push(op);
+				objectStack.push(object);
+				op = '+';
+				object = 0.0;
+				result = ExpressionPriority5();
+				op = opStack.pop();
+				object = objectStack.pop();
+				offset = offsetStack.pop();
+				GetObject();
 
-    public double ExpressionPriority4() {
-        double result = ExpressionPriority3();
+			} else if (expression[offset] == ')') {
+				offsetStack.push(++offset);
+				offset = expression.length;
+				result = object;
 
-        System.out.println("*/");
-        while (op == '*' || op == '/') {
-            if (offset > function.length() - 1) break;
-            switch (op) {
-                case '*':
-                    GetObject();
-                    result = result * ExpressionPriority3();
-                    System.out.println("Multiply, result=" + result + " Offset " + offset);
-                    break;
-                case '/':
-                    GetObject();
-                    result = result / ExpressionPriority3();
-                    System.out.println("Division, result=" + result + " Offset " + offset);
-                    break;
-            }
+			} else {
 
-        }
+				GetObject();
+				result = object;
+			}
+		} else
+			result = object;
+		return result;
+	}
 
-        return result;
-    }
+	private double ExpressionPriority2() {
+		if (noObj && op == '-') {
+			GetObject();
+			return -ExpressionPriority1();
+		}
+		if (noObj && op == '+') {
+			GetObject();
+			return ExpressionPriority1();
+		}
+		return ExpressionPriority1();
+	}
 
-    public double ExpressionPriority5() {
-        double result = 0.0;
-        noObj = true; //Есть ли число. Используется для определения унарного минуса
-        GetObject();
-        result = ExpressionPriority4();
-        while (offset <= function.length() - 1) {
-            System.out.println("Offset=" + offset + " Length=" + function.length());
-            System.out.println("+-");
-            while (op == '+' || op == '-') {
-                if (offset > function.length() - 1) break;
-                switch (op) {
-                    case '+':
-                        GetObject();
-                        result = result + ExpressionPriority4();
-                        System.out.println("Sum, result=" + result + " Offset " + offset);
-                        break;
-                    case '-':
-                        GetObject();
-                        result = result - ExpressionPriority4();
-                        System.out.println("Minus, result=" + result + " Offset " + offset);
-                        break;
+	private double ExpressionPriority3() {
+		double result = ExpressionPriority2();
+		while ((op == '^') || op == 'f') {
+			if (offset > function.length() - 1)
+				break;
+			if (expression[offset] == '^') {
+				offset++;
+				GetObject();
+				double temp = ExpressionPriority2();
+				result = Math.pow(result, temp);
+			} else if (offset < function.length() - 3) {
+				if ("sin".equals(function.substring(offset, offset + 3))) {
+					offset += 3;
+					GetObject();
+					double temp = ExpressionPriority2();
+					result = Math.sin(temp);
+				} else if ("cos".equals(function.substring(offset, offset + 3))) {
+					offset += 3;
+					GetObject();
+					double temp = ExpressionPriority2();
+					result = Math.cos(temp);
+				} else if ("tan".equals(function.substring(offset, offset + 3))) {
+					offset += 3;
+					GetObject();
+					double temp = ExpressionPriority2();
+					result = Math.tan(temp);
+				} else if ("log".equals(function.substring(offset, offset + 3))) {
+					offset += 3;
+					GetObject();
+					double temp = ExpressionPriority2();
+					result = Math.log(temp);
+				} else if (offset < function.length() - 4) {
+					if ("sqrt".equals(function.substring(offset, offset + 4))) {
+						offset += 4;
+						GetObject();
+						double temp = ExpressionPriority2();
+						result = Math.sqrt(temp);
+					}
+				}
+			} else
+				error.add("Invalid function");
 
-                }
+		}
+		return result;
+	}
 
-            }
-        }
-        return result;
-    }
+	private double ExpressionPriority4() {
+		double result = ExpressionPriority3();
+		while (op == '*' || op == '/') {
+			if (offset > function.length() - 1)
+				break;
+			switch (op) {
+			case '*':
+				GetObject();
+				result = result * ExpressionPriority3();
+				break;
+			case '/':
+				GetObject();
+				result = result / ExpressionPriority3();
+				break;
+			}
+
+		}
+
+		return result;
+	}
+
+	private double ExpressionPriority5() {
+		double result = 0.0;
+		noObj = true; // This variable used for unary minus detection
+		GetObject();
+		result = ExpressionPriority4();
+		while (offset <= function.length() - 1) {
+			while (op == '+' || op == '-') {
+				if (offset > function.length() - 1)
+					break;
+				switch (op) {
+				case '+':
+					GetObject();
+					result = result + ExpressionPriority4();
+					break;
+				case '-':
+					GetObject();
+					result = result - ExpressionPriority4();
+					break;
+
+				}
+
+			}
+		}
+		return result;
+	}
 }
--- src/test/java/FunctionCalculatorRunner.java
+++ src/test/java/FunctionCalculatorRunner.java
@@ -9,9 +9,7 @@ public class FunctionCalculatorRunner {
         //"2*(2+3)*2+2"
         //"((1+3*2^(1+1*(1+1-1))))/(1+1-1)" //=13
         //"sin(-1+(2+3*2)*x^(-1+1+1))"  // x=7 // -0.9997551733586199
-        FunctionCalculator func = new FunctionCalculator("s In(     -1+(2+3*2)*X^(-1+1+1))",7.0);
+        FunctionCalculator func = new FunctionCalculator("sin(-1+(2+3*2)*x^(-1+1+1))",7.0);
         System.out.println("RESULT is "+func.Calculator());
-        System.out.println("Braces check:"+func.CheckBraces());
-
 	}
 }
